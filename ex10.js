dcdae51c025bb256b786bb958143b90944a0f918
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-10-29 07:52:25 -0300

    Implement sum of numbers from 1 to 100 using for loop

---
+++ ex10.js
@@ -0,0 +1,8 @@
+function ex10_soma1a100For() {
+    console.log("\n--- Exercício 10: Soma de 1 a 100 (For) ---");
+    let soma = 0;
+    for (let i = 1; i <= 100; i++) {
+        soma += i; // soma = soma + i
+    }
+    console.log(`A soma total de 1 a 100 é: ${soma}`);
+}
