d9b1c5e0b34ea55f059e831d79e86068f978816c
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-10-29 07:52:48 -0300

    Add ex11_arrayComFor function to list fruits

---
+++ ex11.js
@@ -0,0 +1,8 @@
+function ex11_arrayComFor() {
+    console.log("\n--- Exercício 11: Array com For (clássico) ---");
+    const frutas = ["Maçã", "Banana", "Laranja", "Uva", "Manga"];
+    console.log("Lista de Frutas:");
+    for (let i = 0; i < frutas.length; i++) {
+        console.log(frutas[i]);
+    }
+}
