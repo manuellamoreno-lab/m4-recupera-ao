67040cfaca19c4858db30e08cdfc163c246700bf
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-10-29 07:53:13 -0300

    Implement function to log names using for...of

---
+++ ex12.js
@@ -0,0 +1,8 @@
+function ex12_nomesComForOf() {
+    console.log("\n--- Exercício 12: Nomes com For...Of ---");
+    const nomes = ["Ana", "Bruno", "Carla", "Daniel"];
+    console.log("Lista de Nomes:");
+    for (const nome of nomes) {
+        console.log(nome);
+    }
+}
