6605d90f0831c228a239923f49323e4985ae0a9e
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-10-29 07:51:27 -0300

    Add function to count down from 10 to 1

---
+++ ex8.js
@@ -0,0 +1,8 @@
+function ex8_contar10a1DoWhile() {
+    console.log("\n--- Exercício 8: Contar 10 a 1 (Do...While) ---");
+    let i = 10;
+    do {
+        console.log(i);
+        i--;
+    } while (i >= 1);
+}
