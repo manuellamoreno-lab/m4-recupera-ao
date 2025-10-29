997320a28191ea8e61f44b02147ed1d347533b3d
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-10-29 07:51:06 -0300

    Create ex7.js

---
+++ ex7.js
@@ -0,0 +1,8 @@
+function ex7_contar1a5While() {
+    console.log("\n--- Exercício 7: Contar 1 a 5 (While) ---");
+    let i = 1;
+    while (i <= 5) {
+        console.log(i);
+        i++;
+    }
+}
