b81f98b54dde0a94c4dfbb027dd3e7611adf8ced
manuellamoreno-lab <manuella.moreno@aluno.senai.br>
2025-10-29 07:51:57 -0300

    Add function to print even numbers from 0 to 10

---
+++ ex9.js
@@ -0,0 +1,8 @@
+function ex9_pares0a10For() {
+    console.log("\n--- Exercício 9: Pares de 0 a 10 (For) ---");
+    for (let i = 0; i <= 10; i++) {
+        if (i % 2 === 0) {
+            console.log(i);
+        }
+    }
+}
